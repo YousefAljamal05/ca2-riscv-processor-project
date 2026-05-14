f5358dc2d8b3f87edfc4a5b28578faaedfa91876
Yousef Aljamal <yousefaljammal067@gmail.com>
2026-05-14 03:30:22 +0300

    Update ID_EX.v

--- phase2_verilog/ID_Stage/ID_EX.v
+++ phase2_verilog/ID_Stage/ID_EX.v
@@ -1,13 +1,7 @@
-//==========================================================
-// ID/EX PIPELINE REGISTER
-//==========================================================
-// Function: Holds decoded data and control signals safely 
-// for the Execute stage on the next clock cycle.
-//==========================================================
-
 module ID_EX (
     input wire clk,
     input wire reset,
+    input wire flush,
 
     // Control Signals In
     input wire reg_write_in, mem_read_in, mem_write_in, alu_src_in, mem_to_reg_in,
@@ -18,7 +12,10 @@ module ID_EX (
     input wire [63:0] read_data1_in,
     input wire [63:0] read_data2_in,
     input wire [63:0] imm_in,
-    input wire [4:0] rd_in,
+    input wire [4:0]  rs1_in, // NEW for Forwarding
+    input wire [4:0]  rs2_in, // NEW for Forwarding
+    input wire [4:0]  rd_in,
+    input wire predict_taken_in, // NEW for Mispredict check
 
     // Control Signals Out
     output reg reg_write_out, mem_read_out, mem_write_out, alu_src_out, mem_to_reg_out,
@@ -29,11 +26,14 @@ module ID_EX (
     output reg [63:0] read_data1_out,
     output reg [63:0] read_data2_out,
     output reg [63:0] imm_out,
-    output reg [4:0] rd_out
+    output reg [4:0]  rs1_out,
+    output reg [4:0]  rs2_out,
+    output reg [4:0]  rd_out,
+    output reg predict_taken_out
 );
 
     always @(posedge clk) begin
-        if (reset) begin
+        if (reset || flush) begin
             reg_write_out   <= 0;
             mem_read_out    <= 0;
             mem_write_out   <= 0;
@@ -44,7 +44,10 @@ module ID_EX (
             read_data1_out  <= 64'd0;
             read_data2_out  <= 64'd0;
             imm_out         <= 64'd0;
+            rs1_out         <= 5'd0;
+            rs2_out         <= 5'd0;
             rd_out          <= 5'd0;
+            predict_taken_out <= 0;
         end else begin
             reg_write_out   <= reg_write_in;
             mem_read_out    <= mem_read_in;
@@ -56,8 +59,10 @@ module ID_EX (
             read_data1_out  <= read_data1_in;
             read_data2_out  <= read_data2_in;
             imm_out         <= imm_in;
+            rs1_out         <= rs1_in;
+            rs2_out         <= rs2_in;
             rd_out          <= rd_in;
+            predict_taken_out <= predict_taken_in;
         end
     end
-
 endmodule
