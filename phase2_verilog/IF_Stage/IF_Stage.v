67cb1642fd226ba41757f36fe60eb3abb8b5c6d3
Yousef Aljamal <yousefaljammal067@gmail.com>
2026-05-14 03:31:19 +0300

    Update IF_Stage.v

--- phase2_verilog/IF_Stage/IF_Stage.v
+++ phase2_verilog/IF_Stage/IF_Stage.v
@@ -1,39 +1,41 @@
-//==========================================================
-// IF STAGE (Instruction Fetch)
-//==========================================================
-// Function:
-//   - Holds PC
-//   - Fetches instruction from memory
-//   - Computes next PC (PC + 4)
-//
-// Datapath:
-//   PC → Instruction Memory → Instruction
-//   PC + 4 → next PC
-//==========================================================
 module IF_stage (
     input clk,
     input reset,
+    input pc_write,
+    input branch_mispredicted, // From EX
+    input [63:0] correct_next_pc, // From EX
+    input predict_taken, // From Branch Predictor
+    input [63:0] predicted_target, // From Branch Predictor
     output [63:0] pc_out,
     output [31:0] instruction_out
 );
 
     wire [63:0] pc_current;
-    wire [63:0] pc_next;
-    // PC Register
+    reg [63:0] pc_next;
 
     pc PC (
         .clk(clk),
         .reset(reset),
+        .pc_write(pc_write), // ADD THIS TO YOUR PC.v MODULE!
         .next_pc(pc_next),
         .pc(pc_current)
     );
 
-    // Instruction Memory
     instruction_memory IM (
         .pc(pc_current),
         .instruction(instruction_out)
     );
-    // Sequential execution (no branching yet)
-    assign pc_next = pc_current + 64'd4;
+
+    // MUX to decide the Next PC
+    always @(*) begin
+        if (branch_mispredicted) begin
+            pc_next = correct_next_pc; // Recovery from EX
+        end else if (predict_taken) begin
+            pc_next = predicted_target; // Trust the predictor
+        end else begin
+            pc_next = pc_current + 64'd4; // Default sequential
+        end
+    end
+
     assign pc_out = pc_current;
 endmodule
