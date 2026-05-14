79b462aca0a27fdcb3cf50b87241295ebf199633
Yousef Aljamal <yousefaljammal067@gmail.com>
2026-05-14 03:29:18 +0300

    Update IF_ID.v

--- phase2_verilog/IF_Stage/IF_ID.v
+++ phase2_verilog/IF_Stage/IF_ID.v
@@ -10,25 +10,23 @@
 module IF_ID (
     input wire clk,
     input wire reset,
-    
-    // Inputs from IF Stage
+    input wire if_id_write, // Stalls the register
+    input wire flush,       // Clears the register
+
     input wire [63:0] pc_in,
     input wire [31:0] instruction_in,
     
-    // Outputs to ID Stage
     output reg [63:0] pc_out,
     output reg [31:0] instruction_out
 );
 
     always @(posedge clk) begin
-        if (reset) begin
+        if (reset || flush) begin
             pc_out          <= 64'd0;
-            // 0x00000013 is the standard RISC-V NOP (addi x0, x0, 0)
-            instruction_out <= 32'h00000013; 
-        end else begin
+            instruction_out <= 32'h00000013; // NOP
+        end else if (if_id_write) begin
             pc_out          <= pc_in;
             instruction_out <= instruction_in;
         end
     end
-
 endmodule
